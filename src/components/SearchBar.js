00b2054c9381da1ecd597b44da633308c72b51e0
Danthemes <danthemes@gmail.com>
2022-06-21 16:47:26 +0300

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,21 @@ import { eventWrapper } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
+    state = { term: '' };
+
+    onFormSubmit = e => {
+        e.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Label Search:</label>
-                        <input type="text" onChange={this.onInputChange} ></input>
+                        <input type="text" value={this.state.term} onChange={e => { this.setState({term: e.target.value}) }}></input>
                     </div>
                 </form>
             </div>
